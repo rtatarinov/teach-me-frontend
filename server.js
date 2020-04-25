d701c0351ebc2959270c93fefba54b798ac63439
Roman Tatarinov <r.a.tatarinov@yandex.ru>
2020-04-25 22:50:14 +0300

    [App] added https config

--- server.js
+++ server.js
@@ -1,11 +1,32 @@
-const fastify = require('fastify')({
+const redirect = require('fastify')({
   logger: true,
 });
 
 const path = require('path');
 
+const fastify = require('fastify')({
+  logger: true,
+  http2: true,
+  ignoreTrailingSlash: true,
+  allowHTTP1: true,
+  cert: path.join(__dirname, './.ssl/certificate.crt'),
+});
+
 fastify.register(require('fastify-static'), {
   root: path.join(__dirname, 'build'),
+});
+
+const listenCallback = function (err, address) {
+  if (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+
+  fastify.log.info(`server listening on ${address}`);
+};
+
+redirect.all('/*', (req, res) => {
+  res.redirect(`https://${req.hostname}${req.req.url}`);
 });
 
 fastify.get('/', function (req, reply) {
@@ -22,11 +43,5 @@ fastify.setNotFoundHandler(function (request, reply) {
   reply.sendFile('index.html', path.join(__dirname, 'build'));
 });
 
-fastify.listen(80, '0.0.0.0', function (err, address) {
-  if (err) {
-    fastify.log.error(err);
-    process.exit(1);
-  }
-
-  fastify.log.info(`server listening on ${address}`);
-});
+redirect.listen(80, '0.0.0.0');
+fastify.listen(443, '0.0.0.0', listenCallback);
