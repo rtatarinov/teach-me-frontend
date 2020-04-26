be8acea06af3ce4a0cb54362dd2c69c72d661df0
Roman Tatarinov <r.a.tatarinov@yandex.ru>
2020-04-27 00:37:10 +0300

    [App] added https config

--- server.js
+++ server.js
@@ -3,30 +3,19 @@ const redirect = require('fastify')({
 });
 
 const path = require('path');
+const fs = require('fs');
 
 const fastify = require('fastify')({
   logger: true,
   http2: true,
-  ignoreTrailingSlash: true,
-  allowHTTP1: true,
-  cert: path.join(__dirname, './.ssl/certificate.crt'),
+  https: {
+    cert: fs.readFileSync('teachme.crt', 'utf8'),
+    key: fs.readFileSync('teachme.key', 'utf8'),
+  },
 });
 
 fastify.register(require('fastify-static'), {
   root: path.join(__dirname, 'build'),
-});
-
-const listenCallback = function (err, address) {
-  if (err) {
-    fastify.log.error(err);
-    process.exit(1);
-  }
-
-  fastify.log.info(`server listening on ${address}`);
-};
-
-redirect.all('/*', (req, res) => {
-  res.redirect(`https://${req.hostname}${req.req.url}`);
 });
 
 fastify.get('/', function (req, reply) {
@@ -43,5 +32,17 @@ fastify.setNotFoundHandler(function (request, reply) {
   reply.sendFile('index.html', path.join(__dirname, 'build'));
 });
 
+redirect.all('/*', (req, res) => {
+  res.redirect(`https://${req.hostname}${req.req.url}`);
+});
+
 redirect.listen(80, '0.0.0.0');
-fastify.listen(443, '0.0.0.0', listenCallback);
+
+fastify.listen(443, '0.0.0.0', function (err, address) {
+  if (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+
+  fastify.log.info(`server listening on ${address}`);
+});
